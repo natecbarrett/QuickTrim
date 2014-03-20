5bee8ad000bff13dfee1c7bf7bcdf859553e3360
nathan <nathan@bmielite.com>
2014-03-19 20:09:29 -0400

    update

--- upsell_2.php
+++ upsell_2.php
@@ -17,10 +17,10 @@
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" name="viewport">
 <title>Upsell item 2 | QuickTrim Diet and Beauty</title>
 <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'/><![endif]-->
-<link rel="stylesheet" href="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/style.css">
-<link href="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/css/media.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/style.css">
+<link href="css/media.css" rel="stylesheet" type="text/css">
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<!--[if IE 8]><link href="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/css/ie7.css" rel="stylesheet" type="text/css"><![endif]-->
+<!--[if IE 8]><link href="css/ie7.css" rel="stylesheet" type="text/css"><![endif]-->
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
@@ -49,14 +49,14 @@
          <div id="header" class="upsellheader">
              <div class="head clearfix">
                  <div class="head-left">
-                    <img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/upsell-girl-group.png" alt="thanku-girl-group">
+                    <img src="images/upsell-girl-group.png" alt="thanku-girl-group">
                  </div> <!-- head-left ends here-->
                  <div class="head-mid">
-                     <div class="logo"><a href="javascript:void(0);"><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/logo-img.png" alt="logo-img"></a></div>
+                     <div class="logo"><a href="javascript:void(0);"><img src="images/logo-img.png" alt="logo-img"></a></div>
                      <div class="head-main-text">
-                          <img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/upsell-head-main-txt.png" alt="upsell-head-main-txt">
+                          <img src="images/upsell-head-main-txt.png" alt="upsell-head-main-txt">
                      </div>
-                     <div class="whatnext-img"><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/whatnext-img.png" alt="whatnext-img"></div>
+                     <div class="whatnext-img"><img src="images/whatnext-img.png" alt="whatnext-img"></div>
                  </div> <!-- head-mid ends here-->
              </div> <!-- head ends here-->             
          </div> <!-- header ends here-->
@@ -76,12 +76,12 @@
                               <span>reduce belly bloating*</span>
                           </div>
                       </div> <!-- upsell-product-left ends here-->
-                      <div class="upsell-product-right"><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/hanging-special-offer2.png" alt="hanging-special-offer"></div>
+                      <div class="upsell-product-right"><img src="images/hanging-special-offer2.png" alt="hanging-special-offer"></div>
                     </div> <!-- clearfix ends here-->
-                    <div class="upsell-model-img2"><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/upsell-model-img.png" alt="model-img"></div>
-                    <div class="upsell-cellusim-img"><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/fastcleanse-img1.png" alt="fastcleanse-img1"></div>
+                    <div class="upsell-model-img2"><img src="images/upsell-model-img.png" alt="model-img"></div>
+                    <div class="upsell-cellusim-img"><img src="images/fastcleanse-img1.png" alt="fastcleanse-img1"></div>
                  </div> <!-- upsell-product ends here-->
-                 <div class="round-arrow"><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/round-arrow-img.png" alt="round-arrow-img"></div>
+                 <div class="round-arrow"><img src="images/round-arrow-img.png" alt="round-arrow-img"></div>
                  <div class="customer-review">
                      <div class="satisfied-customer">
                         <h2>SATISFIED CUSTOMERS</h2>
@@ -100,52 +100,22 @@
                          <div class="product-price-detail">
 
 
-<form accept-charset='ISO-8859-1' enctype='application/x-www-form-urlencoded;charset=ISO-8859-1' name='opt_in_form' id='opt_in_form' action='https://www.shoponlinesecure.com/index.php?main_page=single_step_upsell' method='post'><input type='hidden' name='limelight_charset' id='limelight_charset' value='ISO-8859-1'/>
+						<form name='opt_in_form' id='opt_in_form' >
+						</form>
 
-<input type="hidden" value="14" name="product_ids[]" />
 
-<input type="hidden" name="invoice_id" value="" />
-<input type="hidden" name="upsell_id" value="2" />
-
-<input type="hidden" name="lander_id" value="" />							
-<input type="hidden" name="fname" value="" />
-<input type="hidden" name="lname" value="" />
-<input type="hidden" name="address" value="" />
-<input type="hidden" name="city" value="" />
-<input type="hidden" name="state" value="" />
-<input type="hidden" name="zipcode" value="" />
-<input type="hidden" name="phone" value="" />
-<input type="hidden" name="email" value="" />
-
-<input type="hidden" name="card_holder" value="" >   
-<input type="hidden" name="CardType" value="">
-<input type="hidden" name="CardNumber" value="" >
-<input type="hidden" name="cardmonth" value="">
-<input type="hidden" name="cardyear" value="">
-<input type="hidden" name="carverirynum" value="" >
-
-<input type="hidden" id="isWebForm" value="1"/>
-      <input type='hidden' id='hasFormSubmitted' value='' /><table cellpadding=2px cellspacing=2px style="width:400px">
-	<input type="hidden" name = "temp_order" id="temp_order" value= "" />
-	<input type="hidden" name = "step_upsell" id="step_upsell" value= "" />
-	<input type='hidden' name = 'product_id' id='product_id' value= "53" />
-	<input type='hidden' name = 'act_up' id='act_up' value='yes' />
-	<input type="hidden" class="upsell_price_x" id="upsell_price_53" name="upsell_custom_price[53]" value="20.00">
-	</form>
-							
-							
                              <span>was</span>
                              <span class="oldprice">$50.00</span>
                              <span class="nowonly">now only</span>
                              <span class="newprice">$20.00</span>
                              <div class="buythisproduct">
 														 	
-															                                <a href="#" id="newupsell_submit" class="newupsell_submit">
-                                   <span><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/icon-cart.png" alt="icon-cart"></span>
+								<a href="#" id="newupsell_submit" class="newupsell_submit">
+                                   <span><img src="images/icon-cart.png" alt="icon-cart"></span>
                                    buy THIS PRODUCT
                                  </a>
                              </div>
-                             <div><img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/big-payment-method-img.jpg" alt="big-payment-method-img"></div>
+                             <div><img src="images/big-payment-method-img.jpg" alt="big-payment-method-img"></div>
                              <div class="clearfix">
 														 
 														 
@@ -154,7 +124,7 @@
                              </div>
                          </div>
                          <div class="final-product-img">
-                             <img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/fastcleanse-img2.png" alt="fastcleanse-img2">
+                             <img src="images/fastcleanse-img2.png" alt="fastcleanse-img2">
                          </div>
                      </div>
                  </div> <!-- customer-review ends here-->
@@ -165,14 +135,14 @@
 	 <div class="clearfix foot-nav">
 			 <ul class="clearfix">
 			 	   
-				<div class="menu-footer-navigation-container"><ul id="menu-footer-navigation" class="menu"><li id="menu-item-24" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-24"><a href="http://www.quicktrimoffer.com/privacy-policy/">Privacy Policy</a></li>
-<li id="menu-item-25" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-25"><a href="http://www.quicktrimoffer.com/?p=66">Terms &#038; Conditions</a></li>
+				<div class="menu-footer-navigation-container"><ul id="menu-footer-navigation" class="menu"><li id="menu-item-24" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-24"><a href="privacy.html">Privacy Policy</a></li>
+<li id="menu-item-25" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-25"><a href="terms.html">Terms &#038; Conditions</a></li>
 <li id="menu-item-27" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-27"><a>Contact Us: 877.264.8633</a></li>
 </ul></div>			 
 			 </ul>
 			 <div class="copyright">
 					 Copyright © 2014. All Rights Reserved<br/>
-					<img src="https://www.quicktrimoffer.com/wp-content/themes/quicktrim/images/payment-method-img.jpg" alt="payment-method">
+					<img src="images/payment-method-img.jpg" alt="payment-method">
 			 </div>
 	 </div>
 	 <p>*This product is not for use by or sale to persons under the age of 18. These products should be used only as directed on the label. It should not be used if you are pregnant or nursing. Consult with a physician before use if you have a serious medical condition or use prescription medications. A Doctor's advice should be sought before using this and any supplemental dietary product. These statements have not been evaluated by the FDA. This product is not intended to diagnose, treat, cure or prevent any disease. Individual weight loss results will vary and conditions may apply.</p>
